6cc88ac1c5d340277bdb0158d4af569b36789320
cintiasil9-crypto <cintiasil9@gmail.com>
2026-02-15 18:12:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,3 @@ def init_db():
     conn.close()
 
     return "Database initialized."
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
