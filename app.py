764c8125a71148dd1298197129a2002675abcc49
cintiasil9-crypto <cintiasil9@gmail.com>
2026-02-17 22:38:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -583,3 +583,7 @@ def home():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
+print("REGISTERED ROUTES:")
+print(app.url_map)
+
